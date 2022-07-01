fe251175c028cbfa638ce829651777cb3c7e9e6b
Alex Gladkov <alexgladkov@icloud.com>
2022-07-01 23:16:13 +0300

    remove

--- build/compileSync/main/productionExecutable/kotlin/TeslaApp.d.ts
+++
@@ -1,5 +0,0 @@
-type Nullable<T> = T | null | undefined
-declare const __doNotImplementIt: unique symbol
-type __doNotImplementIt = typeof __doNotImplementIt
-
-export as namespace TeslaApp;
--- build/ios/IPhone13/TeslaApp.xcodeproj/build/Build/Intermediates.noindex/TeslaApp.build/Debug-iphonesimulator/TeslaApp.build/Script-FE7CEA9673742974A999F3CB.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd "/Users/mobiledeveloper/Documents/Shared/TeslaApp" && ./gradlew -i -p . packComposeUikitApplicationForXCode
